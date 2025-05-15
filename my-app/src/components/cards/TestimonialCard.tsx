7453cca801659e381b239f77e902368d23a0e6cc
sultan93291 <abibdipto@gmail.com>
2025-05-15 14:53:31 +0600

    review dynamic done

--- my-app/src/components/cards/TestimonialCard.tsx
+++ my-app/src/components/cards/TestimonialCard.tsx
@@ -9,6 +9,7 @@ interface TestimonialProps {
   name: string;
   review: string;
   rating: number;
+  isDownPart: boolean;
 }
 
 const TestimonialCard: React.FC<TestimonialProps> = ({
@@ -16,9 +17,8 @@ const TestimonialCard: React.FC<TestimonialProps> = ({
   name,
   review,
   rating,
+  isDownPart,
 }) => {
-
-
   return (
     <div className="h-auto max-w-[457px] rounded-[30px] bg-white border-[1px] border-solid border-[#E2E2E2] py-[31px] px-[30px] flex flex-col gap-y-[60px]  ">
       <div className="flex flex-col gap-y-[18px] ">
@@ -40,35 +40,37 @@ const TestimonialCard: React.FC<TestimonialProps> = ({
           className="text-lg leading-[144%] font-normal text-primary-text-blue   "
         />
       </div>
-      <div className="flex flex-col gap-y-[20.88px]">
-        <hr className="border-t-[1px] border-[#E7E6E6] w-full" />
-        <div className="flex flex-row gap-x-[23.9px] itemsc ">
-          <Heading
-            Variant="h4"
-            Txt={
-              <>
-                <Paragraph
-                  Txt={`${rating}.0`}
-                  className="text-[31px] text-primary-blue leading-[125%]  "
-                />
-                <span className="text-[19px] text-[#B9C8F3] leading-[125%]  ">
-                  /
-                </span>
-                <Paragraph
-                  Txt={"5.0"}
-                  className="text-[19px] text-[#606060] leading-[125%]  "
-                />
-              </>
-            }
-            className="flex flex-row items-center gap-x-1"
-          />
-          <div className="flex flex-row gap-x-1 ">
-            {Array.from({ length: rating }).map((_, i) => (
-              <Star key={i} />
-            ))}
+      {isDownPart && (
+        <div className="flex flex-col gap-y-[20.88px]">
+          <hr className="border-t-[1px] border-[#E7E6E6] w-full" />
+          <div className="flex flex-row gap-x-[23.9px] itemsc ">
+            <Heading
+              Variant="h4"
+              Txt={
+                <>
+                  <Paragraph
+                    Txt={`${rating}.0`}
+                    className="text-[31px] text-primary-blue leading-[125%]  "
+                  />
+                  <span className="text-[19px] text-[#B9C8F3] leading-[125%]  ">
+                    /
+                  </span>
+                  <Paragraph
+                    Txt={"5.0"}
+                    className="text-[19px] text-[#606060] leading-[125%]  "
+                  />
+                </>
+              }
+              className="flex flex-row items-center gap-x-1"
+            />
+            <div className="flex flex-row gap-x-1 ">
+              {Array.from({ length: rating }).map((_, i) => (
+                <Star key={i} />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
