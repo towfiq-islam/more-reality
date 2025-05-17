4cb67f3d825d17ad46a1d71cf417bdecc3eb7337
towfiqislambd <towfiqislambd@gmail.com>
2025-05-17 18:08:00 +0600

    done

--- src/app/commercial/page.tsx
+++ src/app/commercial/page.tsx
@@ -1,27 +1,32 @@
 import CommonHeroBanner from "@/components/CommonHeroBanner/CommonHeroBanner";
-import hero from "@/assests/hero.jpg"
-import Overview from "@/components/CommercialPageComponents/Overview";
+import hero from "@/assests/hero.jpg";
 import Property from "@/components/CommercialPageComponents/Property";
 import WhyChoose from "@/components/CommercialPageComponents/WhyChoose";
 import OurService from "@/components/CommercialPageComponents/OurService";
 import OurSuccess from "@/components/CommercialPageComponents/OurSuccess";
+import overviewImg from "@/assests/commertial/overview.png";
+import SecondaryBanner from "@/components/CommonComponenet/SecondaryBanner";
 
 const page = () => {
-    return (
-        <>
-            <CommonHeroBanner
-                BgImgurl={hero}
-                title="Commercial Real Estate Solutions for Buyers, Sellers & Investors"
-                subTitle="Explore opportunities in office, retail, industrial, and investment properties—backed by expert guidance every step of the way."
-                isInfoBox={false}
-            />
-            <Overview/>
-            <Property/>
-            <WhyChoose/>
-            <OurService/>
-            <OurSuccess/>
-        </>
-    );
+  return (
+    <>
+      <CommonHeroBanner
+        BgImgurl={hero}
+        title="Commercial Real Estate Solutions for Buyers, Sellers & Investors"
+        subTitle="Explore opportunities in office, retail, industrial, and investment properties—backed by expert guidance every step of the way."
+        isInfoBox={false}
+      />
+      <SecondaryBanner
+        title="Overview"
+        description="At MORE Realty, we offer tailored solutions for clients navigating the commercial real estate market. Whether you're looking to buy, sell, lease, or invest, our team of professionals brings deep market insight, strategic guidance, and unmatched service to every transaction."
+        image={overviewImg}
+      />
+      <Property />
+      <WhyChoose />
+      <OurService />
+      <OurSuccess />
+    </>
+  );
 };
 
 export default page;
